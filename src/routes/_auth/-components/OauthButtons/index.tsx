66ac67a4c5d38d98c3dd285be3233b7f7ac661a0
Vladimir <vladimir.timotijevic@dex.rs>
2026-02-14 23:07:10 +0100

    Buttons added on the register page

---
+++ src/routes/_auth/-components/OauthButtons/index.tsx
@@ -0,0 +1,58 @@
+import { useFacebookSignIn, useGoogleSignIn } from '@/api/auth/queries'
+import { Button } from '@/components/custom/Button'
+import { Field } from '@/components/ui/field'
+
+export const OauthButtons = () => {
+  const { mutate: googleLogin, isPending: isGoogleLoginPending } =
+    useGoogleSignIn()
+  const { mutate: facebookLogin, isPending: isFacebookLoginPending } =
+    useFacebookSignIn()
+
+  return (
+    <Field>
+      <Button
+        variant="outline"
+        type="button"
+        loading={{ state: isGoogleLoginPending, text: 'Google prijava...' }}
+        onClick={() => googleLogin()}
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <path
+            d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+            fill="currentColor"
+          />
+        </svg>
+        Google prijava
+      </Button>
+      <Button
+        variant="outline"
+        type="button"
+        loading={{ state: isFacebookLoginPending, text: 'Facebook prijava...' }}
+        onClick={() => facebookLogin()}
+      >
+        <svg
+          fill="currentColor"
+          width="800px"
+          height="800px"
+          viewBox="0 0 1920 1920"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g id="SVGRepo_bgCarrier" stroke-width="0" />
+          <g
+            id="SVGRepo_tracerCarrier"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <g id="SVGRepo_iconCarrier">
+            {' '}
+            <path
+              d="m1416.013 791.915-30.91 225.617h-371.252v789.66H788.234v-789.66H449.808V791.915h338.426V585.137c0-286.871 176.207-472.329 449.09-472.329 116.87 0 189.744 6.205 231.822 11.845l-3.272 213.66-173.5.338c-4.737-.451-117.771-9.25-199.332 65.655-52.568 48.169-79.191 117.433-79.191 205.65v181.96h402.162Zm-247.276-304.018c44.446-41.401 113.71-36.889 118.787-36.663l289.467-.113 6.204-417.504-43.544-10.717C1511.675 16.02 1426.053 0 1237.324 0 901.268 0 675.425 235.206 675.425 585.137v93.97H337v451.234h338.425V1920h451.234v-789.66h356.7l61.932-451.233H1126.66v-69.152c0-54.937 14.214-96 42.078-122.058Z"
+              fill-rule="evenodd"
+            />{' '}
+          </g>
+        </svg>
+        Facebook prijava
+      </Button>
+    </Field>
+  )
+}
